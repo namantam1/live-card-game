2d459d8bbcc8c66dbde861e71c8890e2e23e9267
namantam1 <namantam1@gmail.com>
2026-01-17 14:04:03 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/live-card-game/' : '/',
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+}))
